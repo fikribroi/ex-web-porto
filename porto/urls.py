1c437b9633766209c4bef89f65ca199e0ad3f17b
unknown <muhfikripratama76@gmail.com>
2026-08-01 14:04:38 +0700

    .

--- porto/urls.py
+++ porto/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='main page'),
     path('contact', views.kontak, name='kontak' ),
     path('resume', views.resume, name='resume'),
-    path('projects', views.project),
+    path('projects/', views.project, name='projects'),
     path('signup',views.register, name='register'),
     path('login', views.masuk, name='login'),
     path('logout', views.out),
